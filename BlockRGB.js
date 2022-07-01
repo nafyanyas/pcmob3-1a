2012aeb7d3978a036e37848c152f968b43a192d7
adam.u <ubad.adam@gmail.com>
2022-07-01 21:43:05 +0800

    changed the import text and padding width

--- BlockRGB.js
+++ BlockRGB.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function BlockRGB(props) {
  return (
    <View
      style={{
        backgroundColor: `rgb(${props.red}, ${props.green}, ${props.blue})`,
-       padding: 30,
+       padding: 60,
        width: "100%",
      }}
    ></View>
